2371a68f39982adf9ac09cc43a805697ea3bf7b8
PopovaOlha <fc.odessa.10@gmail.com>
2023-05-07 23:51:14 +0300

    refactor: add a function for copy files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,30 +1,51 @@
 const path = require('path');
 const fs = require('fs');
 
-const filesFolder = path.join(__dirname, 'files');
+const folderOfFiles = path.join(__dirname, 'files');
 const copyFolder = path.join(__dirname, 'files-copy');
 
-console.log(fs);
 
-function deleteDirFiles(dir) {
+const deleteDirFiles = function(dir) {
     fs.readdir(dir, { encoding: 'utf8', withFileTypes: true }, (err, files) => {
         if (err) throw err;
 
         files.map(file => { 
             if (file.isDirectory()) {
             let newDir = path.join(dir, file.name);
-            delFilesInDir(newDir);
+            deleteDirFiles(newDir);
         }
         else {
             fs.unlink(path.join(dir, file.name), () => { });
         }
-    } 
+    }); 
 });
 };
 
-function copyDir(dir, newDir) {
+const copyDir = function(dir, newDir) {
     fs.stat(newDir, (e) => {
         if (!e) {
             deleteDirFiles(newDir);
         }
        });
+       fs.mkdir(newDir, { recursive: true }, (err) => {
+        if (err) throw err;
+
+        fs.readdir(dir, { encoding: 'utf8', withFileTypes: true }, (err, files) => {
+            if (err) throw err;
+
+            files.map(file => {
+                if (file.isDirectory()) {
+                    fs.mkdir(path.join(dir, file.name), { recursive: true }, (error) => { if (error) throw error; });
+                    copyDir(path.join(dir, file.name), path.join(newDir, file.name));
+                }
+                else {
+                    fs.copyFile(path.join(dir, file.name), path.join(newDir, file.name), (errCopy) => {
+                        if (errCopy) throw errCopy;
+                    });
+                }
+            });
+        });
+        console.log('Копия готова!');
+    });
+}
+copyDir(folderOfFiles, copyFolder);
