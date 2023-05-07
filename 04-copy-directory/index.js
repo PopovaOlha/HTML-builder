4a944d9ca60514c7e4dcd5c8ac4039fcf14e43ec
PopovaOlha <fc.odessa.10@gmail.com>
2023-05-07 19:50:53 +0300

    refactor: add a functionality for copy and delete dir

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,3 +3,28 @@ const fs = require('fs');
 
 const filesFolder = path.join(__dirname, 'files');
 const copyFolder = path.join(__dirname, 'files-copy');
+
+console.log(fs);
+
+function deleteDirFiles(dir) {
+    fs.readdir(dir, { encoding: 'utf8', withFileTypes: true }, (err, files) => {
+        if (err) throw err;
+
+        files.map(file => { 
+            if (file.isDirectory()) {
+            let newDir = path.join(dir, file.name);
+            delFilesInDir(newDir);
+        }
+        else {
+            fs.unlink(path.join(dir, file.name), () => { });
+        }
+    } 
+});
+};
+
+function copyDir(dir, newDir) {
+    fs.stat(newDir, (e) => {
+        if (!e) {
+            deleteDirFiles(newDir);
+        }
+       });
