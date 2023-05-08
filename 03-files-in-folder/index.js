249e1408bd9008677a9d5adb9e22eec278fe78ba
PopovaOlha <fc.odessa.10@gmail.com>
2023-05-08 19:35:46 +0300

    refactor: add a functionality that prints data to the console

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -7,5 +7,18 @@ fs.readdir(secretFolder, { encoding: 'utf-8', withFileTypes: true }, (err, files
     if (err) {
         return err;
     }
-    console.log(files);
+
+    files.map(file => {
+        if (!file.isDirectory()) {
+            let fileExt = path.extname(file.name);
+            let nameOfFile = path.basename(file.name, fileExt);
+
+            fs.stat(path.join(secretFolder, file.name), (err, response) => {
+                if (err) {
+                    return err;
+                }
+                console.log(`${nameOfFile} - ${fileExt.replace('.', '')} - ${response.size}kb`);
+           });
+        }
+    })
 })
