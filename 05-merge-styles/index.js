ef335fb096c35eedc897969735b21fa5706d0cba
PopovaOlha <fc.odessa.10@gmail.com>
2023-05-08 22:23:37 +0300

    refactor: add a functional for reading css file and also checked whether the object is a file

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -5,3 +5,20 @@ const pathOfBundle = path.join(__dirname, 'project-dist');
 const pathOfStyle = path.join(__dirname, 'styles');
 const bundle = path.join(pathOfBundle, 'bundle.css');
 console.log(bundle);
+
+const readStyle = function(dir) {
+    fs.readdir(dir, { encoding: 'utf-8', withFileTypes: true }, (err, files) => {
+        if (err) {
+            return err;
+        }
+        files.map(file => {
+            if (path.extname(file.name) === '.css') {
+                if (!file.isDirectory()) {
+                    let p = path.join(dir, file.name);
+                    readStyle(p);
+                }
+            }
+        })
+    })
+
+}
