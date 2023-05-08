77d87adae2de38777dd337e4a37d79412aebdeb5
PopovaOlha <fc.odessa.10@gmail.com>
2023-05-08 22:09:59 +0300

    feat: add a file paths

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,2 +1,7 @@
 const path = require('path');
 const fs = require('fs');
+
+const pathOfBundle = path.join(__dirname, 'project-dist');
+const pathOfStyle = path.join(__dirname, 'styles');
+const bundle = path.join(pathOfBundle, 'bundle.css');
+console.log(bundle);
