a55b70f407640f55289a49c14599dac929814f7c
PopovaOlha <fc.odessa.10@gmail.com>
2023-05-08 16:27:24 +0300

    refactor: add Reading the contents of the secret-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,2 +1,11 @@
 const path = require('path');
 const fs = require('fs');
+const secretFolder = path.join(__dirname, 'secret-folder');
+console.log(secretFolder);
+
+fs.readdir(secretFolder, { encoding: 'utf-8', withFileTypes: true }, (err, files) => {
+    if (err) {
+        return err;
+    }
+    console.log(files);
+})
