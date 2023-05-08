b219f84f1191ba8ce7bd833e59597fcc45de4894
PopovaOlha <fc.odessa.10@gmail.com>
2023-05-08 23:57:58 +0300

    refactor:add an array of style to the new file

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -6,6 +6,19 @@ const pathOfStyle = path.join(__dirname, 'styles');
 const bundle = path.join(pathOfBundle, 'bundle.css');
 console.log(bundle);
 
+const addFile = function(file) {
+    fs.readFile(file, 'utf-8', (err, data) => {
+        if (err) {
+            return err;
+        }
+        fs.appendFile(bundle, `\n/* add from ${path.basename(file)} */\n${data}\n`, (err) => {
+            if (err) {
+                return err;
+            }
+        })
+    });
+};
+
 const readStyle = function(dir) {
     fs.readdir(dir, { encoding: 'utf-8', withFileTypes: true }, (err, files) => {
         if (err) {
@@ -13,12 +26,22 @@ const readStyle = function(dir) {
         }
         files.map(file => {
             if (path.extname(file.name) === '.css') {
-                if (!file.isDirectory()) {
+                if (file.isDirectory()) {
                     let p = path.join(dir, file.name);
                     readStyle(p);
+                } else {
+                    addFile(path.join(dir, file.name));
                 }
             }
-        })
-    })
+        });
+    });
 
-}
+};
+const addBundle = function() {
+    fs.unlink(bundle, () => {
+        fs.rmdir(pathOfBundle, () => {
+            fs.mkdir(pathOfBundle, () => {readStyle(pathOfStyle);})
+        });
+    });
+};
+addBundle();
