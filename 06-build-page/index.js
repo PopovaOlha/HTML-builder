2619149d192f0f3d296f5c57ea85cf0c50bad3aa
PopovaOlha <fc.odessa.10@gmail.com>
2023-05-09 12:25:22 +0300

    feat: add a path for folder

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,2 +1,11 @@
 const path = require('path');
 const fs = require('fs');
+
+const assetsPath = path(__dirname, 'assets');
+const stylesPath = path(__dirname, 'styles');
+const htmlPath = path(__dirname, 'template.html');
+const projectDist = path(__dirname, 'project-dist');
+
+const projectAssets = path(projectDist, 'assets');
+const projectHtml = path(projectDist, 'index.html');
+const projectCss = path(projectDist, 'style.css');
